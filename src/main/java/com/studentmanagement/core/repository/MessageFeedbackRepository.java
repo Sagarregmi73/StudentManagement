2e764294b73f72e0bf6928ae486c926f1f77ebb5
Sagarregmi73 <57311081+Sagarregmi73@users.noreply.github.com>
2023-04-27 12:09:23 +0530

    Delete MessageFeedbackRepository.java

--- src/main/java/com/studentmanagement/core/repository/MessageFeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentmanagement.core.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.studentmanagement.core.model.Applyapplication;
-
-public interface ApplicationRepository extends JpaRepository<Applyapplication, Integer>{
-
-}
